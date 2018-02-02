9113e2017e97f470dca58d1b861d0c2c79254384
Michael Blix <mkeblx@gmail.com>
2018-02-01 18:14:24 -0800

    Get params from command line

--- fragment.js
+++ fragment.js
@@ -2,12 +2,39 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-  var html = '<h1>hello html fragment</h1>';
+  const DEFAULT_FRAG = '<h1>hello html fragment</h1>';
+  const DEFAULT_WIDTH = 500;
+  const DEFAULT_HEIGHT = 500;
+
+  var html = DEFAULT_FRAG;
+  var width = DEFAULT_WIDTH;
+  var height = DEFAULT_HEIGHT;
+
+  process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+  });
+
+  if (process.argv.length > 2) {
+    html = process.argv[2];
+  }
+  console.log(html);
+
+  if (process.argv.length > 3) {
+    width = parseInt(process.argv[3], 10);
+  }
+  if (process.argv.length > 4) {
+    height = parseInt(process.argv[4], 10);
+  }
 
   const browser = await puppeteer.launch({
     headless: true });
 
   const page = await browser.newPage();
+
+  page.setViewport({
+    width: width,
+    height: height
+  });
 
   page.setContent(html);
 
