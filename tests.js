df9edf3c3a461fcfaf586b7db9d0e151e85c26e6
Michael Blix <mkeblx@gmail.com>
2018-02-07 19:07:40 -0800

    Tests init

---
+++ tests.js
@@ -0,0 +1,16 @@
+const { execFile } = require('child_process');
+
+console.log('Running tests...');
+
+var tests = [
+  ['screenshot.js','--url=https://nytimes.com']
+];
+
+execFile('node', tests[0], (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
+
+// console.log('Tests complete.');
