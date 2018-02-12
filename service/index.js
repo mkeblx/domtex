585f6b86f851f05626879a2d23fd320b258189ae
Michael Blix <mkeblx@gmail.com>
2018-02-11 19:48:56 -0800

    Service endpoint working, tmp test response

--- service/index.js
+++ service/index.js
@@ -1,38 +1,47 @@
 var http = require('http');
-var url = require('url');
+var _url = require('url');
 var querystring = require('querystring');
 
+var PORT = 8080;
 const server = http.createServer();
 server.on('request', (request, response) => {
+  console.log('processing request...');
 
   const { method, url } = request;
 
-  var urlParts = url.parse(url, true);
+  var urlParts = _url.parse(url, true);
   var query = urlParts.query;
   var queryParts = querystring.parse(query);
   var siteUrl = queryParts.url;
 
   request.on('error', (err) => {
     console.error(err.stack);
-  });
+  }).on('data', (chunk) => {
 
-  request.on('end', () => {
+  }).on('end', () => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,POST');
 
     var resp = {};
     resp.requestUrl = url;
     resp.url = siteUrl;
     resp.width = 512;
     resp.height = 512;
-    resp.path = 'output/test.png';
+    resp.path = '/output/612c1b08.png';
 
-    response.write(JSON.stringify(resp));
+    var json = JSON.stringify(resp);
+    console.log(json);
+    response.write(json);
 
     response.end();
   })
 
+}).listen(PORT, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
 });
 
-var PORT = 8080;
-server.listen(PORT);
+console.log(`listening on port ${PORT}`);
