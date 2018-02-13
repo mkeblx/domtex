33106e73f4fe51292cd461dd5d1869aac4be7263
Michael Blix <mkeblx@gmail.com>
2018-02-13 12:46:18 -0800

    Add cache command init

--- cache.js
+++ cache.js
@@ -3,18 +3,34 @@
 const fs = require('fs');
 const path = require('path');
 
-var directory = 'output';
+const argv = require('yargs')
+  .usage('Usage $0 command')
+  .demandCommand(1)
+  .argv;
 
-fs.readdir(directory, (err, files) => {
-  if (err) throw err;
+var args = argv._;
+var command = argv._[0];
 
-  for (const file of files) {
-    if (file.charAt(0) == '.')
-      continue;
-    console.log('file: ' + file);
-    fs.unlink(path.join(directory, file), err => {
-      if (err)
-        throw err;
-    });
-  }
-});
+const directory = 'output';
+
+
+if (command == 'clear') {
+  clear();
+}
+
+// clear all
+function clear() {
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      if (file.charAt(0) == '.')
+        continue;
+      console.log('file: ' + file);
+      fs.unlink(path.join(directory, file), err => {
+        if (err)
+          throw err;
+      });
+    }
+  });
+}
