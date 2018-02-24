8ce00818212b78d53d08929964a88470afb52f23
Michael Blix <mkeblx@gmail.com>
2018-02-24 15:14:45 -0800

    Move api page

--- serve.js
+++ serve.js
@@ -3,12 +3,12 @@ const path = require('path');
 var express = require('express');
 var app = express();
 
-var dir = path.join(__dirname, 'samples');
+var dir = path.join(__dirname, '.');
 
 const PORT = 8000;
 
 app.use(express.static(dir));
 
 app.listen(PORT, function () {
-    console.log('Listening on http://localhost:3000/');
+    console.log('Listening on http://localhost:' + PORT);
 });
