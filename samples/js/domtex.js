87f8e9d638f590c88442bb6bac1da3d48f9eb52d
Michael Blix <mkeblx@gmail.com>
2018-03-28 17:45:54 -0700

    Modify UVs to lower # of textures used
    -Added as option can toggle
    -Can share a THREE.Texture
    --not certain if any benefit from WebGL/GPU state changes yet

--- samples/js/domtex.js
+++ samples/js/domtex.js
@@ -3,6 +3,12 @@
 
 var DOMTEX = {};
 
+DOMTEX.cache = {
+  textures: {}
+};
+
+DOMTEX.modifyUVs = true;
+
 DOMTEX.generateRequestUrl = function(params) {
 	var PORT = '8080';
 	var domain = 'http://localhost:'+PORT;
@@ -31,23 +37,56 @@ if (window.THREE) {
 
     var path = tex.path;
     var aspect = tex.width / tex.height;
-    var texture = new THREE.TextureLoader().load( '/'+path );
+    var texture;
+    if (data.url in DOMTEX.cache.textures && DOMTEX.modifyUVs) {
+      texture = DOMTEX.cache.textures[data.url];
+    } else {
+      texture = new THREE.TextureLoader().load( '/'+path );
+      DOMTEX.cache.textures[data.url] = texture;
+    }
 
     if (data.atlas) {
-      texture.repeat.x = tex.width / doc.width;
-      texture.repeat.y = tex.height / doc.height;
+      if (!DOMTEX.modifyUVs) {
+        texture.repeat.x = tex.width / doc.width;
+        texture.repeat.y = tex.height / doc.height;
 
-      var offsetX = tex.x / doc.width;
-      var offsetY = ( ( doc.height - tex.height - tex.y ) / doc.height );
-      texture.offset.x = offsetX;
-      texture.offset.y = offsetY;
-
-      _tex = texture;
-    } else {
-      // generate UVs
+        var offsetX = tex.x / doc.width;
+        var offsetY = ( ( doc.height - tex.height - tex.y ) / doc.height );
+        texture.offset.x = offsetX;
+        texture.offset.y = offsetY;
+      }
     }
 
     return texture;
+  };
+
+  DOMTEX.modUVs = function(geo, data, sel) {
+    var doc = data.textures['document'];
+    var tex = data.textures[sel];
+
+    var texMinX = tex.x / doc.width;
+    var texMinY = tex.y / doc.height;
+    var texMaxX = (tex.x + tex.width) / doc.width;
+    var texMaxY = (tex.y + tex.height) / doc.height;
+    var texWidth = tex.width / doc.width;
+    var texHeight = tex.height / doc.height;
+
+    var offsetX = texMinX;
+    var offsetY = 1 - texMaxY;
+
+    let faces = geo.faceVertexUvs[0];
+    for (let i = 0, n = faces.length; i < n; i++) {
+      let faceUvs = faces[i];
+      for (let k = 0; k < faceUvs.length; k++) {
+        let faceUv = faceUvs[k];
+
+        let x = offsetX + (faceUv.x * texWidth);
+        let y = offsetY + (faceUv.y * texHeight);
+
+        faceUv.x = x;
+        faceUv.y = y;
+      }
+    }
   };
 
   // transform UVs based on repeat & offset
@@ -78,7 +117,10 @@ if (window.THREE) {
       tex.width * s, tex.height * s );
 
     var texture = DOMTEX.createTexture(sel, data);
-    //DOMTEX.transformUVs(geometry, texture);
+
+    if (DOMTEX.modifyUVs) {
+      DOMTEX.modUVs(geometry, data, sel);
+    }
 
     var material = new THREE.MeshBasicMaterial( { map: texture } );
     var object = new THREE.Mesh( geometry, material );
