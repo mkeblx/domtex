063397c30a5f383d38fd06148324653684c58f07
Michael Blix <mkeblx@gmail.com>
2019-11-30 16:38:05 -0800

    Use Aframe npm

--- serve.js
+++ serve.js
@@ -15,6 +15,10 @@ app.get('/examples/js/three.js', function(req, res) {
   res.sendFile(__dirname + '/node_modules/three/build/three.js');
 });
 
+app.get('/examples/js/aframe.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/aframe/dist/aframe-master.min.js');
+});
+
 app.listen(PORT, function () {
     console.log('Listening on http://localhost:' + PORT);
 });
